7cee8642c5f2d63a9492e7beace5481ebb86deb2
xrustik666 <kilorust@gmail.com>
2024-06-02 12:15:12 +0200

    decrease button added. below 0 restriction added

--- week1/prep-exercises/counter-excercise/src/counter.jsx
+++ week1/prep-exercises/counter-excercise/src/counter.jsx
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
 
+function Count({ count }) {
+  return (
+      <p class = "display-count">Count: { count }</p>
+  );
+}
+
+function Button({ setCount }) {
+  const plusClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const minusClick = () => {
+    setCount(prevCount => {
+      if (prevCount <= 0) {
+        return 0;
+      }
+
+      return prevCount - 1;
+    });
+  };
+
+  return (
+    <div>
+      <button class = "plus-button" onClick={plusClick}>Plus 1</button>
+      <button class = "minus-button" onClick={minusClick}>Minus 1</button>
+    </div>
+  );
+}
+
 export function Counter() {
   const [count, setCount] = useState(0);
   const feedback = count > 10 ? "It's higher than 10!" : "Keep counting...";
 
   return (
     <div class="counter">
-      <Count count={count} />
+      <Count count={ count } />
       <Button setCount={setCount} />
       <p>{feedback}</p>
     </div>
-  );
-}
-
-function Count({ count }) {
-  return (
-      <p class = "display-count">Count: {count}</p>
-  );
-}
-
-function Button({ setCount }) {
-  const handleClick = () => {
-    setCount(prevCount => prevCount + 1);
-  };
-
-  return (
-      <button class = "count-button" onClick={handleClick}>Add 1!</button>
   );
 }
